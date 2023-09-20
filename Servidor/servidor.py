5834cbafdbe19d1d0f283206b551cb64072291b5
victor wesley <vctrwesley@gmail.com>
2023-09-20 12:06:17 -0300

    comando sair e hora atual modificados

--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -9,19 +9,35 @@ def informacoes():
    conn.sendall(mensagem)
    print("mensagem enviada com sucesso")
 
+def hora_atual():
+   tempo = datetime.datetime.now()
+   hora_formatada = tempo.strftime("%H:%M:%S")
+   conn.sendall(f"hora atual {hora_formatada}\n".encode())
+
+def nome_arq():
+   pass
+
+def lista_arq():
+   pass
+
+def saindo():
+   conn.sendall("Fechando a conexão".encode())
+   print("ADEUS") 
+   conn.close()
+
 def comandos(command):
    if command == "consulta":
       informacoes()
-"""   elif command == "hora":
+   elif command == "hora":
       hora_atual()
    elif command == "arquivo":
       nome_arq   
    elif command == "listar":
       lista_arq()
    elif command == "sair":
-      adeus()     
+      saindo() 
    else:
-      comando_desconhecido()  """
+      conn.sendall("comando desconhecido".encode())
 
 def identificador_cliente():
    pass
@@ -34,8 +50,6 @@ s.listen()
 print ('Aguardando conexão de um cliente')
 conn, ender = s.accept()
 print ('Conectado em', ender)
-thread = threading.thread(target=identificador_cliente ,args=(conn, ender))
-thread.start()
 while True:
    data = conn.recv(1024)
    command = data.decode()

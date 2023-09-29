cdd0b1d7c06b5cc1b95b8756a7f2669c7afa260d
FelpLiet <felipe.leite23@gmail.com>
2023-09-28 23:42:12 -0300

    correcoes do pull errado

--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -5,19 +5,19 @@ import threading
 import datetime
 import os
 
-def informacoes():
+def informacoes(conn):
    mensagem = HOST + "," + str(PORT)
    conn.sendall(mensagem.encode())
    console.print("mensagem enviada com sucesso", style="#009A05 bold")
 
 
-def hora_atual():
+def hora_atual(conn):
    tempo = datetime.datetime.now()
    hora_formatada = tempo.strftime("%H:%M:%S")
    conn.sendall(f"hora atual {hora_formatada}\n".encode())
 
 
-def dados_arq(nome_arquivo):
+def dados_arq(conn, nome_arquivo):
    diretorio = os.path.dirname(os.path.abspath(__file__))
    pasta_dos_arquivos = os.path.join(diretorio, "Arquivos")
    caminho = os.path.join(pasta_dos_arquivos, nome_arquivo)
@@ -30,36 +30,48 @@ def dados_arq(nome_arquivo):
       conn.sendall("Arquivo nao encontrado".encode())
 
 
-def lista_arq():
+def lista_arq(conn):
    diretorio = os.path.dirname(os.path.abspath(__file__))
    pasta_dos_arquivos = os.path.join(diretorio, "Arquivos")
    arquivos_na_pasta = os.listdir(pasta_dos_arquivos)
    lista = "\n".join(arquivos_na_pasta)
    conn.sendall(f"LISTA DE ARQUIVOS:\n{lista}\n".encode())
 
-def saindo():
+def saindo(conn):
    conn.sendall("Fechando a conexão".encode())
    print("ADEUS")
 
 
-def comandos(command):
+def comandos(conn, command):
    if command == "consulta":
-      informacoes()
+      informacoes(conn)
    elif command == "hora":
-      hora_atual()
+      hora_atual(conn)
    elif command.startswith("arquivo_"):
-      
       nome_arquivo = command.split("_")[1]
       dados_arq(nome_arquivo)         
    elif command == "listar":
-      lista_arq()
+      lista_arq(conn)
    elif command == "sair":
-      saindo() 
+      saindo(conn) 
    else:
       conn.sendall("comando desconhecido".encode())
 
-def identificador_cliente():
-   pass
+def identificador_cliente(conn, ender):
+    console.print("Conectado em", ender, style="#0033D6 bold")
+
+    try:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                console.print(f'Fechando a conexão com {ender}', style="#ff0000 bold")
+                conn.close()
+                break
+            else:
+                command = data.decode()
+                comandos(conn, command)
+    except ConnectionResetError:
+        console.print('Erro de conexão: conexão resetada pelo cliente', style="#ff0000 bold")
 
 console = Console()
 
@@ -70,8 +82,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(ADDR)
 s.listen()
 console.print('Aguardando conexão de um cliente',style="#0033D6 bold")
-conn, ender = s.accept()
-console.print('Conectado em', ender,style="#009A05 bold")
+
 while True:
     conn, addr = s.accept()
     thread = threading.Thread(target=identificador_cliente, args=(conn, addr))

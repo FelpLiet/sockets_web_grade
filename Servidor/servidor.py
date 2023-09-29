7f441c1c1544617ee036c2a5b2fb2ba6cb62eb5d
victor wesley <vctrwesley@gmail.com>
2023-09-29 16:54:29 -0300

    correções para thread e informações do servidor

--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -5,19 +5,17 @@ import threading
 import datetime
 import os
 
-def informacoes():
-   mensagem = HOST + "," + str(PORT)
+def informacoes(conn):
+   mensagem = HOST
    conn.sendall(mensagem.encode())
    console.print("mensagem enviada com sucesso", style="#009A05 bold")
 
-
-def hora_atual():
+def hora_atual(conn):
    tempo = datetime.datetime.now()
    hora_formatada = tempo.strftime("%H:%M:%S")
    conn.sendall(f"hora atual {hora_formatada}\n".encode())
 
-
-def dados_arq(nome_arquivo):
+def dados_arq(conn, nome_arquivo):
    diretorio = os.path.dirname(os.path.abspath(__file__))
    pasta_dos_arquivos = os.path.join(diretorio, "Arquivos")
    caminho = os.path.join(pasta_dos_arquivos, nome_arquivo)
@@ -29,32 +27,29 @@ def dados_arq(nome_arquivo):
    except FileNotFoundError:
       conn.sendall("Arquivo nao encontrado".encode())
 
-
-def lista_arq():
+def lista_arq(conn):
    diretorio = os.path.dirname(os.path.abspath(__file__))
    pasta_dos_arquivos = os.path.join(diretorio, "Arquivos")
    arquivos_na_pasta = os.listdir(pasta_dos_arquivos)
    lista = "\n".join(arquivos_na_pasta)
    conn.sendall(f"LISTA DE ARQUIVOS:\n{lista}\n".encode())
 
-def saindo():
+def saindo(conn):
    conn.sendall("Fechando a conexão".encode())
    print("ADEUS")
 
-
-def comandos(command):
+def comandos(conn, command):
    if command == "consulta":
-      informacoes()
+      informacoes(conn)
    elif command == "hora":
-      hora_atual()
-   elif command.startswith("arquivo_"):
-      
+      hora_atual(conn)
+   elif command.startswith("arquivo_"): 
       nome_arquivo = command.split("_")[1]
-      dados_arq(nome_arquivo)         
+      dados_arq(conn, nome_arquivo)         
    elif command == "listar":
-      lista_arq()
+      lista_arq(conn)
    elif command == "sair":
-      saindo() 
+      saindo(conn) 
    else:
       conn.sendall("comando desconhecido".encode())
 
@@ -63,7 +58,7 @@ def identificador_cliente(conn, ender):
     # Solicita ao cliente que forneça seu nome
     nome_cliente = conn.recv(1024).decode()
     console.print(f"Cliente {nome_cliente} conectado em {ender}", style="#009A05 bold")
-
+    
     try:
         while True:
             data = conn.recv(1024)
@@ -86,11 +81,8 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(ADDR)
 s.listen()
 console.print('Aguardando conexão de um cliente',style="#0033D6 bold")
-conn, ender = s.accept()
-console.print('Conectado em', ender,style="#009A05 bold")
+
 while True:
     conn, addr = s.accept()
     thread = threading.Thread(target=identificador_cliente, args=(conn, addr))
     thread.start()
-    #ta funcionando ;3
-
